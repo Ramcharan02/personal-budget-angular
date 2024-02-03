866d0c489b2e84cd245d4f2208d19ab22e627a7a
Ram charan <ramcharan@Rams-MBP.lan>
2024-02-03 15:19:25 -0500

    "Loading data from JSON file"

--- server.js
+++ server.js
@@ -1,35 +1,20 @@
-
 const express = require('express');
+const fs = require('fs'); // Import the 'fs' module
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
 app.get('/hello', (req, res) => {
     res.send("hello World");
 });
 
-app.get('/budget',(req,res) => {
-    res.json(budget)
-})
-
-
+app.get('/budget', (req, res) => {
+    fs.readFile('data.json', 'utf8', (data) => {
+            const budgetData = JSON.parse(data);
+            res.json(budgetData);
+    });
+});
 
 app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`);
