356efa54155ec1637cd68e12326cc394e2eb0efb
Ram charan <ramcharan@Rams-MBP.lan>
2024-02-03 15:02:12 -0500

    "Added server, Axios, and ChartJS"

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-// Budget API
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use('/', express.static('public'));
 
 const budget = {
     myBudget: [
@@ -23,11 +21,15 @@ const budget = {
         },
     ]
 };
-
-
-app.get('/budget', (req, res) => {
-    res.json(budget);
+app.get('/hello', (req, res) => {
+    res.send("hello World");
 });
+
+app.get('/budget',(req,res) => {
+    res.json(budget)
+})
+
+
 
 app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`);
